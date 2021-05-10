51c4ada44862381cabbe61370d1974043560c0bf
Arun <arunkumarmohan1@gmail.com>
2021-05-10 14:46:07 +0530

    Hamcrest update

---
+++ src/test/java/com/example/mockito/business/HamcrestMatchTest.java
@@ -0,0 +1,22 @@
+package com.example.mockito.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestMatchTest {
+
+    @Test
+    public void hamcrest_Test(){
+        List<Integer> list= Arrays.asList(1,3,4);
+        assertThat(list,hasSize(3));
+        assertThat(list,hasItems(3,4));
+
+        assertThat("",emptyString());
+        assertThat("Arune",startsWith("A"));
+    }
+}
